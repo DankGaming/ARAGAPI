9b1726b1467a4104230cdfe501a5ef8ce9bd2bf5
newo-2001 <o.elderbroek@gamil.com>
2020-10-07 12:34:47 +0200

    Initialized nodejs

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import { port } from "./config";
+
+const app: express.Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}!`);
+});
