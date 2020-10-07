0ec45ae8d84b98d316ee1836d15037a3890b3d0d
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-10-07 14:30:09 +0200

    Dockerfile and docker compose

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:14.0
+
+RUN npm install -g npm
+
+WORKDIR /app
+COPY package.json ./
+# RUN npm cache clean --force && rm -rf node_modules
+# RUN npm cache verify
+# RUN npm install --verbose && npm install tsc -g
+# RUN npm install
+
+COPY . .
+
+# CMD ["npm", "run", "dev"]
+
+CMD ["/bin/bash"]
--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,12 @@ import { port } from "./config";
 const app: express.Application = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("sdsd");
+})
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}!`);
+    console.log(`Server started on port ${port}`);
 });
