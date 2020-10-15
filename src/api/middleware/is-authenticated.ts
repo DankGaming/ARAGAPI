8882ec45f1bbfe81da712d79b1569b7cb3991daa
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-10-15 10:33:51 +0200

    Authentication middleware

--- src/api/middleware/is-authenticated.ts
+++ src/api/middleware/is-authenticated.ts
@@ -1,14 +1,53 @@
 import { Request, Response, NextFunction } from "express";
+import { UnauthorizedException } from "../../exceptions/UnauthorizedException";
+import jsonwebtoken from "jsonwebtoken";
+import { Employee } from "../components/employee/employee.model";
+import * as employeeDAO from "../components/employee/employee.dao";
+import { decode } from "punycode";
+
+declare global {
+    namespace Express {
+        export interface Request {
+            employee: Employee;
+        }
+    }
+}
+
+interface DecodedToken {
+    employee: Employee;
+    iat: number;
+}
 
 export async function isAuthenticated(
     req: Request,
     res: Response,
     next: NextFunction
 ) {
-    // Get JWT
-    // Check if JWT is valid
-    // Get employeeID from JWT
-    // Request employee from employeeDAO
-    // Add employee to req.employee
-    // Call next
+    try {
+        if (!req.headers.authorization) throw new UnauthorizedException();
+        const privateKey = process.env.JWT_SECRET;
+        if (!privateKey) throw new Error("JWT secret must be defined");
+
+        // Get JWT
+        const token = req.headers.authorization.split(" ")[1];
+
+        // Check if JWT is valid
+        const decodedToken: DecodedToken = jsonwebtoken.verify(
+            token,
+            privateKey
+        ) as DecodedToken;
+
+        const employeeInToken: Employee = decodedToken.employee;
+
+        const employee: Employee = await employeeDAO.findByID(
+            employeeInToken.id
+        );
+
+        // Add employee to request object
+        req.employee = employee;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
 }
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,12 +3,13 @@ import nodeRoutes from "./components/node/node.routes";
 import treeRoutes from "./components/tree/tree.routes";
 import employeeRoutes from "./components/employee/employee.routes";
 import authRoutes from "./components/auth/auth.routes";
+import { isAuthenticated } from "./middleware/is-authenticated";
 
 const router: Router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/nodes", nodeRoutes);
 router.use("/trees", treeRoutes);
-router.use("/employees", employeeRoutes);
+router.use("/employees", isAuthenticated, employeeRoutes);
 
 export default router;
