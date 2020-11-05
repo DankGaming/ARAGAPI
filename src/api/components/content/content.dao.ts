31071ea57d28967c07c3d219b37ecff59515220a
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-11-05 10:34:44 +0100

    Releasing connection after update statements

--- src/api/components/content/content.dao.ts
+++ src/api/components/content/content.dao.ts
@@ -87,8 +87,10 @@ export const update = async (
             );
 
         await connection.commit();
+        connection.release();
     } catch (err) {
         await connection.rollback();
+        connection.release();
         throw new InternalServerException();
     }
 };
